6c4af64acbb6fd04a8fc38322e886332510b2e89
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-03-09 23:09:53 +0530

    Add. Style & CSS loaders to webpack config file

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-import { stylesToBody, stylesToHeader } from "./styles";
-
-const header = document.querySelector("header");
-
-const helloWithName = (name) => {
-    const htmlTemplate = `
-        <h1>Hello there, ${name}!!
-    `;
-
-    header.innerHTML = htmlTemplate;
-};
-
-helloWithName("Ironman");
-stylesToBody();
-stylesToHeader();
+console.log("Hello world!");
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
                     presets: ["@babel/preset-env"]
                 }
             }
+        },
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
         }]
     }
 };
