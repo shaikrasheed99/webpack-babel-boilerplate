f3367f1ecfec15022500feae66d8464000cb5016
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-03-09 19:13:37 +0530

    Add. Script to run webpack

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,8 +1,1 @@
-"use strict";
-
-var header = document.querySelector("header");
-var helloWithName = function helloWithName(name) {
-  var htmlTemplate = "\n        <h1>Hello there, ".concat(name, "!!\n    ");
-  header.innerHTML = htmlTemplate;
-};
-helloWithName("Ironman");
+document.querySelector("header").innerHTML="\n        <h1>Hello there, Ironman!!\n    ";
